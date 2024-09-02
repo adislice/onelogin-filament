15e72a70ac2b2d038c2b51b143ce6e1eb87516aa
R. Adi Permana <radipermana29@gmail.com>
2024-09-02 20:46:45 +0700

    Setup filament

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('photo')->nullable();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('role', ['admin', 'employee', 'user'])->default('user');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
